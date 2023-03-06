1d7457a76eec5965508a11d878f682f061a621ca
KaIr0sG <114661969+KaIr0sG@users.noreply.github.com>
2023-03-06 23:32:44 +0100

    Add files via upload

---
+++ ProyectoDI_AD/AccDatos/DummyManager.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace AccDatos
+{
+    public class DummyManager
+    {
+        AccesoDatos aData;
+        //Campos privados
+        private int _filas;
+        private int _columnas;
+        private double _p; //probabilidad nacimiento arbol en tierra fertil
+        private double _q; //probabilidad combustion espontanea de arbol
+
+        private char[,] _tableroActual;
+        private char[,] _tableroSiguiente;
+        //"x" Tierra NO fertil
+        //"_" Tierra fertil
+        //"o" Arbol
+        //"*" Arbol en llamas
+        //"w" Agua/rio
+
+        //Propiedades publicas
+        public double P
+        {
+            get
+            {
+                return _p;
+            }
+            set
+            {
+                _p = value;
+                //[TO DO]: Binding para visualizar
+            }
+        }
+        public double Q
+        {
+            get
+            {
+                return _q;
+            }
+            set
+            {
+                _q = value;
+                //[TO DO]: Binding para visualizar
+            }
+        }
+
+        //Constructor(es)
+        public DummyManager()
+        {
+            aData = new AccesoDatos();
+
+        }
+
+        //Metodos
+
+        //public void CambiarApellidoActor(int id_actor, string nuevoApellido)
+        //{
+        //    aData.SP_UpdateActor(id_actor, nuevoApellido);
+        //}
+
+        public void Login(string usuario, string pass)
+        {
+            int respuesta = aData.SP_Login(usuario, Md5(pass));
+            if (respuesta == 0)
+            {
+                MessageBox.Show("Bienvenid@");
+            }
+            else if (respuesta == -1)
+            {
+                MessageBox.Show("No existe este usuario en la base de datos");
+            }
+            else if (respuesta == -2)
+            {
+                MessageBox.Show("Contraseña incorrecta");
+            }
+            else
+            {
+                MessageBox.Show("Error desconocido");
+            }
+        }
+        public Empleado VerDatosEmpleado(int idEmpleado)
+        {
+            aData.SP_StaffDetail(idEmpleado);
+
+            return null;
+        }
+
+        public void AltaEmpleado(string nombre, string apellido, string mail, string usuario, string pass)
+        {
+            if (aData.SP_AddStaff(nombre, apellido, mail, usuario, Md5(pass)) == 0)
+            {
+                MessageBox.Show("El empleado ha sido dado de alta CORRECTAMENTE");
+            }
+        }
+        public int CargarDatosNuevaSimulacion(int filas, int columnas, double p, double q)
+        {
+            _filas = filas;
+            _columnas = columnas;
+            P = p;
+            Q = q;
+
+            _tableroActual = new char[_filas, _columnas];
+            _tableroSiguiente = new char[_filas, _columnas];
+
+            //Primera generación vacía por defecto (celdas fertiles)
+            for (int i = 0; i < _filas; i++)
+                for (int j = 0; j < _columnas; j++)
+                {
+                    _tableroActual[i, j] = '_';
+                    _tableroSiguiente[i, j] = '_';
+                }
+
+            return 0;
+        }
+        public int CargarEjemploPrestablecido()
+        {
+            _filas = 3;
+            _columnas = 3;
+            P = 0.001;
+            Q = 0.01;
+
+            _tableroActual = new char[_filas, _columnas];
+            _tableroSiguiente = new char[_filas, _columnas];
+
+            _tableroActual[0, 0] = '_';
+            _tableroActual[0, 1] = 'w';
+            _tableroActual[0, 2] = '_';
+            _tableroActual[1, 0] = 'o';
+            _tableroActual[1, 1] = '*';
+            _tableroActual[1, 2] = 'x';
+            _tableroActual[2, 0] = 'w';
+            _tableroActual[2, 1] = 'o';
+            _tableroActual[2, 2] = 'w';
+
+            for (int i = 0; i < _filas; i++)
+                for (int j = 0; j < _columnas; j++)
+                {
+                    _tableroSiguiente[i, j] = '_';
+                }
+
+
+            return 0;
+        }
+        public Grid PintarTablero()
+        {
+            Grid tablero = new Grid();
+
+            tablero.Width = 600;
+            tablero.Height = 600;
+            //tablero.ShowGridLines = true;
+
+            for (int i = 0; i < _columnas; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                tablero.ColumnDefinitions.Add(cd);
+            }
+            for (int i = 0; i < _filas; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                tablero.RowDefinitions.Add(rd);
+            }
+
+            // Casilla Vacia para cada rincon 
+            Uri imgUri = new Uri("./img/fertil.png", UriKind.Relative);
+            for (int i = 0; i < _filas; i++)
+            {
+                for (int j = 0; j < _columnas; j++)
+                {
+                    Image item = new Image();
+                    item.Name = "c_" + i + "_" + j;
+                    item.Source = new BitmapImage(imgUri);
+                    item.Margin = new Thickness(1);
+                    Grid.SetRow(item, i);
+                    Grid.SetColumn(item, j);
+                    tablero.Children.Add(item);
+                }
+            }
+
+            return tablero;
+        }
+        public Grid PintarTableroModelo()
+        {
+            Grid tablero = new Grid();
+
+            tablero.Width = 600;
+            tablero.Height = 600;
+
+            for (int i = 0; i < _columnas; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                tablero.ColumnDefinitions.Add(cd);
+            }
+            for (int i = 0; i < _filas; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                tablero.RowDefinitions.Add(rd);
+            }
+            for (int i = 0; i < _filas; i++)
+            {
+                for (int j = 0; j < _columnas; j++)
+                {
+                    Image item = new Image();
+                    item.PreviewMouseDown += new MouseButtonEventHandler(click_celda);
+                    Uri imgUri = new Uri("./img/fertil.png", UriKind.Relative);
+                    if (_tableroActual[i, j] == '_')
+                        imgUri = new Uri("./img/fertil.png", UriKind.Relative);
+
+                    if (_tableroActual[i, j] == 'x')
+                        imgUri = new Uri("./img/nofertil.png", UriKind.Relative);
+
+                    if (_tableroActual[i, j] == 'o')
+                        imgUri = new Uri("./img/arbol.png", UriKind.Relative);
+
+                    if (_tableroActual[i, j] == '*')
+                        imgUri = new Uri("./img/fuego.png", UriKind.Relative);
+
+                    if (_tableroActual[i, j] == 'w')
+                        imgUri = new Uri("./img/agua.gif", UriKind.Relative);
+
+                    item.Name = "c_" + i + "_" + j;
+                    item.Source = new BitmapImage(imgUri);
+                    item.Margin = new Thickness(1);
+                    Grid.SetRow(item, i);
+                    Grid.SetColumn(item, j);
+                    tablero.Children.Add(item);
+
+                }
+            }
+
+            return tablero;
+        }
+        private void click_celda(object sender, RoutedEventArgs e)
+        {
+            Image celda_clickada = (Image)e.Source;
+            Uri imgUri = new Uri("./img/fertil.png", UriKind.Relative);
+            //c_0_0
+            int i = int.Parse(celda_clickada.Name.Split('_')[1]);
+            int j = int.Parse(celda_clickada.Name.Split('_')[2]);
+
+            //cambiar el estado de una celda (irlo rotando...)
+            if (_tableroActual[i, j] == 'x')
+            {
+                _tableroActual[i, j] = '_';
+                imgUri = new Uri("./img/fertil.png", UriKind.Relative);
+                celda_clickada.Source = new BitmapImage(imgUri);
+            }
+            else if (_tableroActual[i, j] == '_')
+            {
+                _tableroActual[i, j] = 'o';
+                imgUri = new Uri("./img/arbol.png", UriKind.Relative);
+                celda_clickada.Source = new BitmapImage(imgUri);
+            }
+            else if (_tableroActual[i, j] == 'o')
+            {
+                _tableroActual[i, j] = '*';
+                imgUri = new Uri("./img/fuego.png", UriKind.Relative);
+                celda_clickada.Source = new BitmapImage(imgUri);
+            }
+            else if (_tableroActual[i, j] == '*')
+            {
+                _tableroActual[i, j] = 'w';
+                imgUri = new Uri("./img/agua.gif", UriKind.Relative);
+                celda_clickada.Source = new BitmapImage(imgUri);
+            }
+            else if (_tableroActual[i, j] == 'w')
+            {
+                _tableroActual[i, j] = 'x';
+                imgUri = new Uri("./img/nofertil.png", UriKind.Relative);
+                celda_clickada.Source = new BitmapImage(imgUri);
+            }
+        }
+        public void Avanzar1()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+
+            for (int i = 0; i < _filas; i++)
+            {
+                for (int j = 0; j < _columnas; j++)
+                {
+                    //Si la casilla tiene suelo infértil,
+                    //la siguiente iteración pasará a ser
+                    //de suelo fértil si tenía al lado suelo fértil.
+                    if (_tableroActual[i, j] == 'x')
+                    {
+                        if (((i - 1 >= 0) && _tableroActual[i - 1, j] == '_') ||
+                            ((i + 1 < _filas) && _tableroActual[i + 1, j] == '_') ||
+                            ((j - 1 >= 0) && _tableroActual[i, j - 1] == '_') ||
+                            ((j + 1 < _columnas) && _tableroActual[i, j + 1] == '_')
+                            )
+                        {
+                            _tableroSiguiente[i, j] = '_';
+                        }
+                    }
+                    //Si la casilla tiene suelo fértil, existe una probabilidad P
+                    //de que la siguiente iteración nazca un árbol.
+                    if (_tableroActual[i, j] == '_')
+                    {
+                        if (r.NextDouble() < P)
+                            _tableroSiguiente[i, j] = 'o';
+                        else
+                            _tableroSiguiente[i, j] = '_';
+                    }
+                    //Si la casilla tiene árbol, existe una probabilidad Q de que el árbol prenda espontáneamente(durante la siguiente iteración, esa casilla será de fuego)
+                    if (_tableroActual[i, j] == 'o')
+                    {
+                        if (((i - 1 >= 0) && _tableroActual[i - 1, j] == '*') ||
+                            ((i + 1 < _filas) && _tableroActual[i + 1, j] == '*') ||
+                            ((j - 1 >= 0) && _tableroActual[i, j - 1] == '*') ||
+                            ((j + 1 < _columnas) && _tableroActual[i, j + 1] == '*')
+                            )
+                        {
+                            _tableroSiguiente[i, j] = '*';
+                        }
+                        else
+                        {
+                            if (r.NextDouble() < Q)
+                                _tableroSiguiente[i, j] = '*';
+                            else
+                                _tableroSiguiente[i, j] = 'o';
+                        }
+
+                    }
+
+                    //Si la casilla está en fuego, la siguiente iteración será de suelo infértil y las casillas adyacentes que tengan árbol, se prenderán en la siguiente iteración.
+                    if (_tableroActual[i, j] == '*')
+                    {
+                        //Las casillas adyacentes a los ríos
+                        //NUNCA pueden estar en estado infértil,
+                        //como poco estarán en estado fértil.
+                        if (((i - 1 >= 0) && _tableroActual[i - 1, j] == 'w') ||
+                            ((i + 1 < _filas) && _tableroActual[i + 1, j] == 'w') ||
+                            ((j - 1 >= 0) && _tableroActual[i, j - 1] == 'w') ||
+                            ((j + 1 < _columnas) && _tableroActual[i, j + 1] == 'w')
+                            )
+                        {
+                            _tableroSiguiente[i, j] = '_';
+                        }
+                        else
+                        {
+                            _tableroSiguiente[i, j] = 'x';
+                        }
+                    }
+                    if (_tableroActual[i, j] == 'w')
+                    {
+                        _tableroSiguiente[i, j] = 'w';
+                    }
+
+                }
+            }
+            for (int i = 0; i < _filas; i++)
+                for (int j = 0; j < _columnas; j++)
+                    _tableroActual[i, j] = _tableroSiguiente[i, j];
+
+        }
+
+        public static string Md5(string input, bool isLowercase = false)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var byteHash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var hash = BitConverter.ToString(byteHash).Replace("-", "");
+                return (isLowercase) ? hash.ToLower() : hash;
+            }
+        }
+    }
+}
---
+++ ProyectoDI_AD/AccDatos/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccDatos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DummyManager manager;
+        public MainWindow()
+        {
+            InitializeComponent();
+            manager = new DummyManager();
+
+            DataContext = manager;
+            //Las funciones para los void
+
+            OcultarTodo();
+            MostrarLogIn();
+
+        }
+        //Aqui ocultamos todo 
+        void OcultarTodo()
+        {
+            sp_FormRegistro.Visibility = Visibility.Collapsed;
+            sp_LOGIN.Visibility = Visibility.Collapsed;
+            sp_PreparacionSimulacion.Visibility = Visibility.Collapsed;
+        }
+        //Aqui mostramos el login 
+        void MostrarLogIn()
+        {
+            sp_LOGIN.Visibility = Visibility.Visible;
+            // sp_Inicio.Visibility = Visibility.Collapsed;
+
+        }
+        //Aqui mostramos el registro
+        void MostrarRegistro()
+        {
+            sp_FormRegistro.Visibility = Visibility.Visible;
+            sp_LOGIN.Visibility = Visibility.Collapsed;
+        }
+        // Nos enseña la simulacion y nos oculta el login y el registro
+        private void MostrarPreparacionSimulacion()
+        {
+            sp_PreparacionSimulacion.Visibility = Visibility.Visible;
+            sp_FormRegistro.Visibility = Visibility.Collapsed;
+            sp_LOGIN.Visibility = Visibility.Collapsed;
+            // sp_ivitado.Visibility = Visibility.Collapsed;
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    manager.CambiarApellidoActor(int.Parse(tb_idActor.Text), tb_nuevoApellido.Text);
+        //}
+
+        private void Button_Click_DatosStaff(object sender, RoutedEventArgs e)
+        {
+            manager.VerDatosEmpleado(1);
+        }
+        // Nos confirma si el usuario se ha dado de alta o no
+        private void Button_Click_Alta(object sender, RoutedEventArgs e)
+        {
+            if (tb_pass.Password == tb_passConfirm.Password)
+            {
+
+
+                manager.AltaEmpleado(tb_nombre.Text,
+                                     tb_apellido.Text,
+                                     tb_mail.Text,
+                                     //int.Parse(tb_tiendaEmpleado.Text),
+                                     tb_usuario.Text,
+                                     tb_pass.Password
+
+                                     );
+            }
+            else
+            {
+                MessageBox.Show("La contraseña no coincide con el campo de confrimación. Vuelva a intentarlo");
+            }
+        }
+        // Nos oculta todo y nos muestra el registro
+        private void bt_registro_Click(object sender, RoutedEventArgs e)
+        {
+            OcultarTodo();
+            MostrarRegistro();
+        }
+
+        //  Los TextBox que tenemos, nos los mandamos al manager que va a la BBDD
+        private void bt_login_Click(object sender, RoutedEventArgs e)
+        {
+            manager.Login(tb_usuario.Text, pb_pass.Password);
+        }
+
+        // Inicio de invitado
+        private void bt_usuarioAnonimo_Click(object sender, RoutedEventArgs e)
+        {
+            OcultarTodo();
+            MostrarPreparacionSimulacion();
+        }
+       // Al iniciar el tablero nos dara el tablero que indicamos 
+        private void bt_iniciarTablero_Click(object sender, RoutedEventArgs e)
+        {
+            manager.CargarDatosNuevaSimulacion(int.Parse(tb_filas.Text),
+                                               int.Parse(tb_columnas.Text),
+                                               double.Parse(tb_P.Text),
+                                               double.Parse(tb_Q.Text));
+
+            sp_Tablero.Children.Clear();
+
+            sp_Tablero.Children.Add(manager.PintarTableroModelo());
+
+            sp_Tablero.Visibility = Visibility.Visible;
+
+        }
+        // Boton avanzar
+        private void Avanzar1_Click(object sender, RoutedEventArgs e)
+        {
+            manager.Avanzar1();
+
+            sp_Tablero.Children.Clear();
+            sp_Tablero.Children.Add(manager.PintarTableroModelo());
+        }
+        // Ocultamos el tablero y la simulacion y enseñamos el login
+        private void bt_Cerrar_Sesion(object sender, RoutedEventArgs e)
+        {
+            sp_Tablero.Visibility = Visibility.Collapsed;
+            sp_PreparacionSimulacion.Visibility = Visibility.Collapsed;
+            sp_LOGIN.Visibility = Visibility.Visible;
+        }
+        // Ocultamos el Login y mostramos el registro
+        private void bt_Registrarse_Click(object sender, RoutedEventArgs e)
+        {
+            sp_FormRegistro.Visibility = Visibility.Visible;
+            sp_LOGIN.Visibility = Visibility.Collapsed;
+        }
+
+        public static class AnimationHelper
+        {
+            private static void AnimateWidth(DependencyObject target, double from, double to)
+            {
+                var WidthAnimation = new DoubleAnimation
+                {
+                    From = 80,
+                    To = 120,
+                    Duration = TimeSpan.FromSeconds(2)
+                };
+
+                Storyboard.SetTarget(WidthAnimation, target);
+                Storyboard.SetTargetProperty(WidthAnimation, new PropertyPath("Width"));
+               
+
+                var storyboard = new Storyboard();
+                storyboard.Children.Add(WidthAnimation);
+                storyboard.Begin();
+            }
+
+            /// <summary>
+            /// Fades in the given dependency object.
+            /// </summary>
+            /// <param name="target">The target dependency object to fade in.</param>
+            public static void FadeIn(DependencyObject target)
+            {
+                AnimateWidth(target, 0, 1);
+            }
+        }
+
+    }
+    }
